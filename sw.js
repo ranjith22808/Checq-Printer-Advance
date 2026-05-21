078fd90669faa09a2985c8c8f2a13e72a795c4cf
ranjith22808 <ranjith22808@gmail.com>
2026-05-21 22:32:43 +0530

    3 FILE CONCEPET

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'checq-cache-v3.3.8';
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['index.html', 'manifest.json', './']);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((hit) => {
+      return hit || fetch(evt.request).catch(() => {
+        if (evt.request.mode === 'navigate') {
+          return caches.match('index.html') || caches.match('./');
+        }
+      });
+    })
+  );
+});
